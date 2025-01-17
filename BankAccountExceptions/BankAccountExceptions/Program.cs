7a5f2035945dd5a5123add48626a5dd9c83cbe2d
Gustavo Edrafox <111284644+gustavoEdrafox@users.noreply.github.com>
2025-01-17 19:08:44 -0300

    Exercise with exceptions

---
+++ BankAccountExceptions/BankAccountExceptions/Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+
+using System.Globalization;
+using BankAccountExceptions.Entities;
+
+try
+{
+    Console.WriteLine("Enter account data:");
+
+    Console.Write("Number: ");
+    int number = int.Parse(Console.ReadLine());
+
+    Console.Write("Holder: ");
+    string holder = Console.ReadLine();
+
+    Console.Write("Initial balance: ");
+    double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+    Console.Write("Withdraw limit: ");
+    double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+    Account acc = new Account(number, holder, balance, withdrawLimit);
+
+    Console.Write("Enter amount for withdraw: ");
+    double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    acc.Withdraw(amount);
+
+
+    Console.WriteLine("New balance: " + acc.Balance);
+
+}
+catch (Exception e)
+{
+    Console.WriteLine("Withdraw error:  " + e.Message);
+}
